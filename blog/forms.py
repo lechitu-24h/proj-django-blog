5a8815524d5f805a4797a04539f52e7bb1527e1e
lechitu <lechitu1103@gmail.com>
2021-08-06 17:39:34 +0700

    Add search, filter posts function, make pretty

--- blog/forms.py
+++ blog/forms.py
@@ -9,10 +9,3 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
-
-
-class LoginForm(forms.ModelForm):
-
-    class Meta:
-        model = User
-        fields = ('username', 'password',)
--- blog/views.py
+++ blog/views.py
@@ -4,23 +4,18 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
-from .forms import LoginForm
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django import forms
 
 
 def login(request):
     if request.method == "POST":
-        form = LoginForm(request.POST)
         user = authenticate(
             request, username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             auth_login(request, user)
             return redirect('post_list')
-        else:
-            return render(request, 'blog/login.html', {'form': form})
-    else:
-        form = LoginForm()
-    return render(request, 'blog/login.html', {'form': form})
+    return render(request, 'blog/login.html')
 
 
 def logout(request):
@@ -30,9 +25,31 @@ def logout(request):
 
 def post_list(request):
     if request.user.is_authenticated:
-        posts = Post.objects.filter(
-            published_date__lte=timezone.now()).order_by('published_date')
-        return render(request, 'blog/post_list.html', {'posts': posts})
+        search = [
+            {
+                'title': 'Title',
+                'value': 'title'
+            },
+            {
+                'title': 'Content',
+                'value': 'text'
+            }
+        ]
+
+        if request.method == "POST" and request.POST['searchText']:
+            searchText = request.POST['searchText']
+            searchWith = request.POST['searchWith']
+            posts = Post.objects.filter(
+                published_date__lte=timezone.now()).order_by('published_date')
+            if searchWith == "text":
+                posts = posts.filter(text__contains=searchText)
+            else:
+                posts = posts.filter(title__contains=searchText)
+            return render(request, 'blog/post_list.html', {'posts': posts, 'searchText': searchText, 'search': search, 'searchWith': searchWith})
+        else:
+            posts = Post.objects.filter(
+                published_date__lte=timezone.now()).order_by('published_date')
+            return render(request, 'blog/post_list.html', {'posts': posts, 'search': search})
     else:
         return redirect('login')
 
